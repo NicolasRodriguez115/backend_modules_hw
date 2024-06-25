f4643a4df482a5bc0e4725d597881e3d0ed2b0d7
NicolasRodriguez115 <nr115935@gmail.com>
2024-06-25 16:45:34 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,4 @@
+import mood
+user_mood = input(f"How are you feeling today?\n1. Happy\n2. Sad\n3. Normal\n")
+
+mood.mood_response(user_mood)
---
+++ mood.py
@@ -0,0 +1,10 @@
+def mood_response(mood):
+    if mood == "1":
+        print("Hope that feeling lasts long!") 
+    elif mood == "2":
+        print("It's okay to be sad once in a while. But remeber, it will get better!")
+    elif mood == "3":
+        print("Use this moment to reflect and be prepared for whatever may come in the future, be it good or bad.")
+    else:
+        print("Not a valid input.")
+    
