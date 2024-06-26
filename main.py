41d2f39247b20d77bca35bc2c0006946563d2792
NicolasRodriguez115 <nr115935@gmail.com>
2024-06-26 16:38:16 -0400

    Added a for loop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import mood
-user_mood = input(f"How are you feeling today?\n1. Happy\n2. Sad\n3. Normal\n")
-
-mood.mood_response(user_mood)
+while True:
+    user_mood = input(f"How are you feeling today?\n1. Happy\n2. Sad\n3. Normal\n4. quit\n")
+    if user_mood == "4":
+        break
+    mood.mood_response(user_mood)
